6d937bba827d47898e668913474cd022adce2953
mustang2247 <mustang2247@gmail.com>
2017-04-01 00:07:28 +0800

    update

--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 )
 
 func main() {
 	log.Print("Starting server")
 
-
 	//httpservice.InitHttpService()
 
-
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello!!!!!"))
-	})
-
-	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello aa"))
-	})
-
-	log.Fatal(http.ListenAndServe(":8888", nil))
 }
--- routers/httpservice/httpservice.go
+++ routers/httpservice/httpservice.go
@@ -3,18 +3,27 @@ package httpservice
 import (
 	"log"
 	"net/http"
-
-	"macaron"
+	// "macaron"
 )
 
 func InitHttpService() {
-	m := macaron.Classic()
-	m.Get("/", myHandler)
 
-	log.Println("Server is running...")
-	log.Println(http.ListenAndServe("0.0.0.0:4000", m))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!!!!!"))
+	})
+
+	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello aa"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8888", nil))
+	// m := macaron.Classic()
+	// m.Get("/", myHandler)
+
+	// log.Println("Server is running...")
+	// log.Println(http.ListenAndServe("0.0.0.0:4000", m))
 }
 
-func myHandler(ctx *macaron.Context) string {
-	return "the request path is: " + ctx.Req.RequestURI
-}
+// func myHandler(ctx *macaron.Context) string {
+// 	return "the request path is: " + ctx.Req.RequestURI
+// }
