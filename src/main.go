371bb30fe7688dbb231d0f23af07d5e525fcf926
mustang2247 <mustang2247@gmail.com>
2017-03-31 20:28:45 +0800

    Update

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+)
+
+func main() {
+	log.Print("Starting server")
+
+
+	//httpservice.InitHttpService()
+
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte("Hello!!!!!"))
+	//})
+	//
+	//log.Fatal(http.ListenAndServe(":8888", nil))
+}
